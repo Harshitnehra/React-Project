5661ac45564cddf809f4ab3fca862c91c9c10d42
Harshit nehra <nehraharshit01@gmail.com>
2026-02-05 08:53:47 +0530

    Add bg changer React project

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <button onClick={() => {setcolor("blue")}} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}}>blue</button>
         <button onClick={() => {setcolor("yellow")}} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "yellow"}}>yellow</button>
         <button onClick={() => {setcolor("pink")}} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "pink"}}>pink</button>
+        <button onClick={() => {setcolor("white")}} className="outline-none px-4 py-1 rounded-full text-green shadow-lg" style={{backgroundColor: "white"}}>white</button>
         <button onClick={() => {setcolor("orange")}} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "orange"}}>orange</button>
       </div>
     </div>
